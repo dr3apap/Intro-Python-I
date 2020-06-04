3c7db655d40e809741068823e303a2ba2e1e61a8
Adebola <dr3apap@gmail.com>
2020-06-04 14:35:52 -0700

    first five exercises done

--- src/03_modules.py
+++ src/03_modules.py
@@ -4,28 +4,35 @@ which allows you to access many system specific variables and
 methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
-
-import sys
-# See docs for the sys module: https://docs.python.org/3.7/library/sys.html
-
-# Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
-
-# Print out the OS platform you're using:
-# YOUR CODE HERE
-
-# Print out the version of Python you're using:
-# YOUR CODE HERE
-
-
-import os
-# See the docs for the OS module: https://docs.python.org/3.7/library/os.html
-
-# Print the current process ID
-# YOUR CODE HERE
-
-# Print the current working directory (cwd):
-# YOUR CODE HERE
-
-# Print out your machine's login name
-# YOUR CODE HERE
+import getpass                                                                  
+import os                                                                       
+import sys                                                                      
+# See docs for the sys module: https://docs.python.org/3.7/library/sys.html     
+                                                                                
+# Print out the command line arguments in sys.argv, one per line:               
+# YOUR CODE HERE                                                                   
+print(sys.argv, "\n")                                                              
+# Print out the OS platform you're using:                                          
+# YOUR CODE HERE                                                                   
+print(sys.platform)                                                                
+print(sys.version.split('/'))                                                      
+# Print out the OS platform you're using:                                          
+# Print out the version of Python you're using:                                    
+# YOUR CODE HERE                                                                
+                                                                                
+                                                                                
+# See the docs for the OS module: https://docs.python.org/3.7/library/os.html   
+                                                                                
+# Print the current process ID                                                  
+# YOUR CODE HERE                                                                
+pid = os.getpid()                                                               
+print(pid)                                                                      
+# Print the current working directory (cwd):                                    
+# YOUR CODE HERE                                                                
+cwd = os.getcwd()                                                               
+print(cwd)                                                                      
+                                                                                
+# Print out your machine's login name                                           
+# YOUR CODE HERE                                                                
+user = getpass.getuser()                                                        
+print(user)                  
